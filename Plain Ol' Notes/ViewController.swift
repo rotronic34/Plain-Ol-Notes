4473b5f0d98d1bf53d93384246bb993817c70924
Roeut <roeut.chak@gmail.com>
2019-09-26 18:44:26 -0400

    added NavigationViewController; added tableView "+" buttonBar and edit mode to delete rows.

--- Plain Ol' Notes/ViewController.swift
+++ Plain Ol' Notes/ViewController.swift
@@ -15,10 +15,25 @@ class ViewController: UIViewController, UITableViewDataSource {
     @IBOutlet weak var table: UITableView!
     var data = ["Item 1", "Item 2", "Item 3"]
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-       table.dataSource = self
+        table.dataSource = self
+        self.title = "Notes"
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNote))
+        self.navigationItem.rightBarButtonItem = addButton
+        self.navigationItem.leftBarButtonItem = editButtonItem
+    }
+    
+    //MARK: @objc
+    @objc func addNote() {
+        if table.isEditing {
+            return
+        }
+        let name = "Item \(data.count+1)"
+        data.insert(name, at: 0)
+        let indexPath:IndexPath = IndexPath(row: 0, section: 0)
+        table.insertRows(at: [indexPath], with: .automatic)
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -28,8 +43,22 @@ class ViewController: UIViewController, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell")!
         cell.textLabel?.text = data[indexPath.row]
-        return cell 
-        
+        return cell
+    }
+    
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        table.setEditing(editing, animated: animated)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        data.remove(at: indexPath.row)
+        table.deleteRows(at: [indexPath], with: .fade)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        //dispose of any resources that can be recreated
     }
 
 }
